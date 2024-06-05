881f65db7a57a2b68e28d98ed41906a688bd31f1
SamSapiens <siemprees@outlook.com>
2024-06-05 14:52:01 -0500

    agregue tables

--- analysis/analisisAire.py
+++ analysis/analisisAire.py
@@ -1,12 +1,15 @@
 import pandas as pd 
 
 from data.generators.generadorAire import generarDatosCalidadAire
+from helpers.crearTablaHTML import crearTabla
 
 def construirAireDataFrame():
     datosAires=generarDatosCalidadAire()
 
 
-    aireDataFrame = pd.DataFrame(datosAires,columns=['nombre', ])
+    aireDataFrame = pd.DataFrame(datosAires,columns=['nombre', 'comuna', 'ica', 'fecha', 'correo' ])
+
+    crearTabla(aireDataFrame,"datosAire")
 
     print(aireDataFrame)
 
